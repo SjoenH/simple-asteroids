7286e325a1e5916df6cb8d08466c97362b36deef
Henry Sjøen <henry.sjoen@miles.no>
2023-05-14 20:54:33 +0200

    Dumb frontend

--- src/main.ts
+++ src/main.ts
@@ -1,391 +1,197 @@
-// import * as signalR from '@microsoft/signalr';
+import * as signalR from '@microsoft/signalr';
 
-import { Application, Sprite, Texture, Ticker } from 'pixi.js';
+import { Application, Sprite, Ticker } from 'pixi.js';
 
 const app: Application = new Application();
 document.body.appendChild(app.view as HTMLCanvasElement);
 
-const playerSprite: Sprite = Sprite.from('/assets/Player.png');
-
-// center anchor
-playerSprite.anchor.set(0.5);
-app.stage.addChild(playerSprite);
-
-const asteroidSprite: Sprite = Sprite.from('/assets/Asteroid.png');
-
-type Player = {
-  id: string;
-  position: {
-    x: number;
-    y: number;
-  };
-  acceleration: {
-    x: number;
-    y: number;
-  };
-  alive: boolean;
-  rotation: number;
-};
-type Bullet = {
-  id: string;
-  position: {
-    x: number;
-    y: number;
-  };
-  acceleration: {
-    x: number;
-    y: number;
-  };
-  alive: boolean;
-  rotation: number;
+const assets: Record<string, string> = {
+  player: '/assets/Player.png',
+  asteroid: '/assets/Asteroid.png',
+  bullet: '/assets/Bullet.png',
+  star: '/assets/Star.png'
 };
 
+const sprites: Record<string, Sprite> = {};
 
-const players: Player[] = [];
-players.push({
-  id: '1234',
-  position: {
-    x: 100,
-    y: 100,
-  },
-  acceleration: {
-    x: 0,
-    y: 0,
-  },
-  alive: true,
-  rotation: 0,
-});
+/**
+ * Adds a sprite to the stage and stores it in the sprites object
+ * @param asset the path to the asset you want to add
+ * @param spriteName the name you want to give the sprite
+ * @param x initial x position
+ * @param y initial y position
+ * @param size the size of the sprite (1 = 100%)
+ */
+function addSprite(asset: string, spriteName: string, x: number, y: number, size: number = 1) {
+  const sprite: Sprite = Sprite.from(asset);
+  sprite.anchor.set(0.5);
+  app.stage.addChild(sprite);
+  sprites[spriteName] = sprite;
+  sprite.x = x;
+  sprite.y = y;
+  sprite.scale.set(size);
+  return sprite;
+}
 
-let mousePosition = {
-  x: 0,
-  y: 0,
-};
+/**
+ * Removes a sprite from the stage and deletes it from the sprites object
+ * @param id the name of the sprite you want to remove
+ */
+function removeSprite(id: string) {
+  app.stage.removeChild(sprites[id]);
+  delete sprites[id];
+}
 
-const asteroids = [
-  {
-    id: Math.random().toString(),
-    position: {
-      x: Math.random() * 1000,
-      y: Math.random() * 1000,
-    },
-    acceleration: {
-      x: Math.random() * 10,
-      y: Math.random() * 10,
-    },
-    alive: true,
-    rotation: 0,
-  },
-  {
-    id: Math.random().toString(),
-    position: {
-      x: Math.random() * 1000,
-      y: Math.random() * 1000,
-    },
-    acceleration: {
-      x: Math.random() * 10,
-      y: Math.random() * 10,
-    },
-    alive: true,
-    rotation: 0,
-  },
-  {
-    id: Math.random().toString(),
-    position: {
-      x: Math.random() * 1000,
-      y: Math.random() * 1000,
-    },
-    acceleration: {
-      x: Math.random() * 10,
-      y: Math.random() * 10,
-    },
-    alive: true,
-    rotation: 0,
+/**
+ * Updates the position of a sprite if it exists, otherwise adds it to the stage
+ * @param id 
+ * @param x 
+ * @param y 
+ */
+function upsertSprite(id: string, x: number, y: number, asset = assets.player) {
+  if (sprites[id]) {
+    sprites[id].x = x;
+    sprites[id].y = y;
+  } else {
+    addSprite(asset, id, x, y);
   }
+}
 
-];
-const bullets: Bullet[] = [];
+function addMainPlayer(id: string = 'player1',
+  x: number = 100, y: number = 100) {
+  const player = addSprite(assets.player, id, x, y);
+  // color it yellow
+  player.tint = 0xffff00;
+}
 
-const keysPressed = new Map<string, boolean>();
+function addOtherPlayer(id: string = 'player2',
+  x: number = 400, y: number = 400) {
+  const p = addSprite(assets.player, id, x, y);
+  // color it red
+  p.tint = 0xff0000;
+}
+function addAsteroid(id: string = 'asteroid1',
+  x: number = 200, y: number = 200, size: number = 1) {
+  addSprite(assets.asteroid, 'asteroid', x, y, size);
+}
 
-const tankId = '1234';
+function addBullet(id: string = 'bullet1', x: number = 300, y: number = 300) {
+  addSprite(assets.bullet, id, x, y);
+}
 
-// Note, we do not modify the sprite directly, since we want to only update every frame
-document.addEventListener('keydown', (e) => {
-  const tank = players.find((t) => t.id === tankId);
-  if (!tank) return;
-
-  switch (e.code) {
-    case 'ArrowUp':
-      keysPressed.set('ArrowUp', true);
-      break;
-    case 'ArrowDown':
-      keysPressed.set('ArrowDown', true);
-      break;
-    case 'ArrowLeft':
-      keysPressed.set('ArrowLeft', true);
-      break;
-    case 'ArrowRight':
-      keysPressed.set('ArrowRight', true);
-      break;
-    case 'Space':
-      keysPressed.set('Space', true);
-      break;
-    default:
-      break;
+const stars: Sprite[] = [];
+function addBackground() {
+  // Sprinkle the background with some stars
+  for (let i = 0; i < 100; i++) {
+    const x = Math.random() * app.screen.width;
+    const y = Math.random() * app.screen.height;
+    const star = addSprite(assets.star, `star${i}`, x, y, 0.5);
+    stars.push(star); // so we can animate them later
   }
-});
+}
+// startGame: Starts the game by loading assets, initializing sprites, and adding the game loop logic.
+async function startGame(): Promise<void> {
+  // Add some test sprites
+  addBackground();
+  addMainPlayer();
+  addAsteroid();
+  addBullet();
+  addOtherPlayer();
 
-document.addEventListener('keyup', (e) => {
-  const tank = players.find((t) => t.id === tankId);
-  if (!tank) return;
+  // If you want to, you could do some custom animation or logic inside the game loop
+  Ticker.shared.add(() => {
+    // Note, that this front-end is super simple and dumb,
+    // so we don't have any game logic and all we do is update sprites when we get events from the server.
 
-  switch (e.code) {
-    case 'ArrowUp':
-      keysPressed.set('ArrowUp', false);
-      break;
-    case 'ArrowDown':
-      keysPressed.set('ArrowDown', false);
-      break;
-    case 'ArrowLeft':
-      keysPressed.set('ArrowLeft', false);
-      break;
-    case 'ArrowRight':
-      keysPressed.set('ArrowRight', false);
-      break;
-    case 'Space':
-      keysPressed.set('Space', false);
-      break;
-    default:
-      break;
-  }
-});
+    // But this could be a good place to do it if we wanted the front-end to do some of the game logic.
 
-
-// rotate to follow mouse
-document.addEventListener('mousemove', (e) => {
-  mousePosition = {
-    x: e.clientX,
-    y: e.clientY,
-  };
-});
-
-// Add all the sprites to the stage
-app.stage.addChild(playerSprite);
-asteroids.forEach(element => {
-  const a = app.stage.addChild(asteroidSprite);
-  a.x = element.position.x;
-  a.y = element.position.y;
-  a.rotation = element.rotation;
-});
-
-const bulletSprite = new Sprite(Texture.WHITE);
-bulletSprite.width = 10;
-bulletSprite.height = 10;
-bulletSprite.tint = 0xffffff;
-
-Ticker.shared.add((acceleration) => {
-  // Update asteroid position
-  asteroids.forEach((asteroid) => {
-    const { acceleration: asteroidAcceleration, position: asteroidPosition } = asteroid;
-    // asteroidPosition.x += asteroidAcceleration.x;
-    // asteroidPosition.y += asteroidAcceleration.y;
-
-    // Update sprite position and rotation
-    asteroidSprite.x = asteroidPosition.x;
-    asteroidSprite.y = asteroidPosition.y;
-    asteroidSprite.rotation = asteroid.rotation;
-
-    const { width, height } = app.screen;
-    // if outside of screen, wrap
-    if (asteroidPosition.x > width) {
-      asteroidPosition.x = 0;
-    }
-    if (asteroidPosition.x < 0) {
-      asteroidPosition.x = width;
-    }
-    if (asteroidPosition.y > height) {
-      asteroidPosition.y = 0;
-    }
-    if (asteroidPosition.y < 0) {
-      asteroidPosition.y = height;
-    }
-
+    // But animating the stars is not part of the game logic, so we can do that here:
+    stars.forEach((star) => {
+      // We can use the FPS to throttle the animation 
+      // so it doesn't impact performance on slower devices
+      const fps = Ticker.shared.FPS;
+      if (fps > 120) star.alpha = Math.random() * 0.5 + 0.5;
+    });
 
   });
+}
 
-  // Update player position
-  const tank = players.find((t) => t.id === tankId);
-  if (tank) {
-    const { acceleration: tankAcceleration, position: tankPosition } = tank;
-    if (keysPressed.get('ArrowUp')) {
-      // tankAcceleration.y -= 1;
-      // Move in the rotation direction
-      tankAcceleration.x += Math.sin(tank.rotation) * 0.1;
-      tankAcceleration.y -= Math.cos(tank.rotation) * 0.1;
-    }
-    if (keysPressed.get('ArrowDown')) {
-      // tankAcceleration.y += 1;
-      // Move in the rotation direction
-      tankAcceleration.x -= Math.sin(tank.rotation) * 0.1;
-      tankAcceleration.y += Math.cos(tank.rotation) * 0.1;
-    }
-    if (keysPressed.get('ArrowLeft')) {
-      // tankAcceleration.x -= 1;
-      tank.rotation -= 0.05;
-    }
-    if (keysPressed.get('ArrowRight')) {
-      // tankAcceleration.x += 1;
-      tank.rotation += 0.05;
-    }
+startGame();
 
-    // Space to shoot
-    if (keysPressed.get('Space')) {
-      // spawn bullet
-      const bullet = {
-        id: Math.random().toString(),
-        position: {
-          x: tankPosition.x,
-          y: tankPosition.y,
-        },
-        acceleration: {
-          x: Math.sin(tank.rotation) * 10,
-          y: -Math.cos(tank.rotation) * 10,
-        },
-        alive: true,
-        rotation: tank.rotation,
-      };
-      bullets.push(bullet);
+///
+//  SignalR - Connection and Event Handling
+///
+const connection = new signalR.HubConnectionBuilder()
+  .withUrl('https://localhost:7159/game', {
+    skipNegotiation: true,
+    transport: signalR.HttpTransportType.WebSockets
+    // transport: signalR.HttpTransportType.LongPolling
+  }).build();
 
-      bulletSprite.anchor.set(0.5);
-      bulletSprite.x = bullet.position.x;
-      bulletSprite.y = bullet.position.y;
-      bulletSprite.rotation = bullet.rotation;
-      app.stage.addChild(bulletSprite);
-    }
+connection.start().then(() => {
 
-    // update bullets
-    bullets.forEach((bullet) => {
-      const { acceleration: bulletAcceleration, position: bulletPosition } = bullet;
-      bulletPosition.x += bulletAcceleration.x;
-      bulletPosition.y += bulletAcceleration.y;
+  // Todo: Handle connection events
+  // Here are some examples of events you could handle
 
-      // Update sprite position and rotation
-      bulletSprite.x = bulletPosition.x;
-      bulletSprite.y = bulletPosition.y;
-      bulletSprite.rotation = bullet.rotation;
+  // When the player joins, we need to add the sprite
+  connection.on('playerJoined', (id: string) => {
+    console.log('playerJoined', id);
+    // Note, that we don't need to add the main player, since it's already added
+    addOtherPlayer(id);
+  });
 
-      const { width, height } = app.screen;
-      // if outside of screen, wrap
-      if (bulletPosition.x > width) {
-        bulletPosition.x = 0;
-      }
+  // When the player leaves, we need to remove the sprite
+  connection.on('playerLeft', (id: string) => {
+    console.log('playerLeft', id);
+    removeSprite(id);
+  });
 
-      if (bulletPosition.x < 0) {
-        bulletPosition.x = width;
-      }
+  connection.on('playerMoved', (id: string, x: number, y: number) => {
+    console.log('playerMoved', id, x, y);
+    upsertSprite(id, x, y);
+  });
 
-      if (bulletPosition.y > height) {
-        bulletPosition.y = 0;
-      }
+  connection.on('asteroidMoved', (id: string, x: number, y: number) => {
+    console.log('asteroidMoved', id, x, y);
+    upsertSprite(id, x, y, assets.asteroid);
+  });
 
-      if (bulletPosition.y < 0) {
-        bulletPosition.y = height;
-      }
-    });
+  connection.on('bulletMoved', (id: string, x: number, y: number) => {
+    console.log('bulletMoved', id, x, y);
+    upsertSprite(id, x, y, assets.bullet);
+  });
+
+  // When the bullet is removed, we need to remove the sprite
+  // This happens when the bullet hits an asteroid or goes off screen
+  connection.on('bulletRemoved', (id: string) => {
+    console.log('bulletRemoved', id);
+    removeSprite(id);
+  });
+
+  // When the asteroid is removed, we need to remove the sprite
+  // This happens when the asteroid is hit by a bullet
+  connection.on('asteroidRemoved', (id: string) => {
+    console.log('asteroidRemoved', id);
+    removeSprite(id);
+  });
 
 
-    tankPosition.x += tankAcceleration.x;
-    tankPosition.y += tankAcceleration.y;
+  // When the player is killed, we need to remove the sprite
+  connection.on('playerKilled', (id: string) => {
+    console.log('playerKilled', id);
+    removeSprite(id);
+    // We could also add an explosion animation here
+    // and then remove the explosion animation when the player respawns
+  });
 
-    // Update sprite position and rotation
-    playerSprite.x = tankPosition.x;
-    playerSprite.y = tankPosition.y;
-    playerSprite.rotation = tank.rotation;
+  // When the player respawns, we need to update the sprite
+  // Respawn is when the player is killed and then comes back to life
+  connection.on('playerRespawned', (id: string, x: number, y: number) => {
+    console.log('playerRespawned', id, x, y);
+    upsertSprite(id, x, y);
+  });
 
-    // if outside of screen, wrap around
-    if (tankPosition.x < 0) {
-      tankPosition.x = app.screen.width;
-    }
-
-    if (tankPosition.x > app.screen.width) {
-      tankPosition.x = 0;
-    }
-
-    if (tankPosition.y < 0) {
-      tankPosition.y = app.screen.height;
-    }
-
-    if (tankPosition.y > app.screen.height) {
-      tankPosition.y = 0;
-    }
-
-    const speedLimit = 5;
-    // limit speed
-    if (tankAcceleration.x > speedLimit) {
-      tankAcceleration.x = speedLimit;
-    }
-    if (tankAcceleration.y > speedLimit) {
-      tankAcceleration.y = speedLimit;
-    }
-
-    if (tankAcceleration.x < -speedLimit) {
-      tankAcceleration.x = -speedLimit;
-    }
-    if (tankAcceleration.y < -speedLimit) {
-      tankAcceleration.y = -speedLimit;
-    }
-  }
-
-  // If collision, reset position
-  if (tank) {
-    const { position: tankPosition } = tank;
-    asteroids.forEach((asteroid) => {
-      const { position: asteroidPosition } = asteroid;
-      if (Math.abs(tankPosition.x - asteroidPosition.x) < 20 && Math.abs(tankPosition.y - asteroidPosition.y) < 20) {
-        tankPosition.x = 0;
-        tankPosition.y = 0;
-      }
-    });
-  }
-})
-
-
-
-// const connection = new signalR.HubConnectionBuilder()
-//   .withUrl('https://localhost:7159/game', {
-//     skipNegotiation: true,
-//     transport: signalR.HttpTransportType.WebSockets
-//     // transport: signalR.HttpTransportType.LongPolling
-//   }).build();
-
-// connection.start().then(() => {
-//   console.log('connected');
-//   connection.send('SendMessage', "Hello from client");
-//   connection.on('ReceiveMessage', () => {
-//     // const existingTank = tanks.find((t) => t.id === tank.id);
-//     // if (existingTank) {
-//     //   existingTank.position = tank.position;
-//     //   existingTank.acceleration = tank.acceleration;
-//     //   existingTank.rotation = tank.rotation;
-//     // } else {
-//     //   tanks.push(tank);
-//     // }
-//     console.log("Received message from server");
-//   })
-// }).catch((err) => {
-//   console.error(err);
-// }
-// );
-
-
-// connection.on('ReceiveMessage', () => {
-//   // const existingTank = tanks.find((t) => t.id === tank.id);
-//   // if (existingTank) {
-//   //   existingTank.position = tank.position;
-//   //   existingTank.acceleration = tank.acceleration;
-//   //   existingTank.rotation = tank.rotation;
-//   // } else {
-//   //   tanks.push(tank);
-//   // }
-//   console.log("Received message from server");
-// });
+}).catch((err) => {
+  console.error(err);
+}
+);
